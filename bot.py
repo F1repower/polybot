eea1fb1cd6176551b36d585a6bb0e21ffa0f051a
F1repower <patrin.ai@mail.ru>
2020-04-19 14:52:58 +0300

    make it better

--- bot.py
+++ bot.py
@@ -1,42 +1,9 @@
 import random
 import requests
 import telebot
-import utils
-import config
 
 # main variables
-TOKEN = "1106845063:AAFUWishJZFfzOL9YWElj6GrXjfeLiWxUWs"
 bot = telebot.TeleBot('1106845063:AAFUWishJZFfzOL9YWElj6GrXjfeLiWxUWs')
-bot = telebot.TeleBot(config.telebot_token)
-
-
-class BotHandler:
-
-    def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot1106845063:AAFUWishJZFfzOL9YWElj6GrXjfeLiWxUWs/".format(token)
-
-    def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = { 'timeout': timeout, 'offset': offset }
-        resp = requests.get(self.api_url + method, params)
-        result_json = resp.json()['result']
-        return result_json
-
-    def send_message(self, chat_id, text):
-        params = { 'chat_id': chat_id, 'text': text }
-        method = 'sendMessage'
-        resp = requests.post(self.api_url + method, params)
-        return resp
-
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-        else:
-            last_update = get_result[None]
-        return last_update
 
 
 @bot.message_handler(commands=['start', 'go'])
